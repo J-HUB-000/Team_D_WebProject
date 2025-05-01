6ab5d202d0f8c8eb5baa234f1d50869dcad9c291
AJS <AJS@Slim3-AJS.mshome.net>
2025-05-01 13:06:18 +0900

    작업할 프로젝트, 여기에 만들겁니다.

---
+++ Team_D_project/src/main/java/ce/mnu/team_d/TeamDProjectApplication.java
@@ -0,0 +1,13 @@
+package ce.mnu.team_d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDProjectApplication.class, args);
+	}
+
+}
---
+++ Team_D_project/src/test/java/ce/mnu/team_d/TeamDProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ce.mnu.team_d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
